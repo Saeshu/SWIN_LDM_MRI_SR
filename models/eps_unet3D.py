4005549ec34086d9fe9bcf33fea4c3a120a571c9
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-01-02 13:17:33 +0530

    Update eps_unet3D.py

--- models/eps_unet3D.py
+++ models/eps_unet3D.py
@@ -1,12 +1,37 @@
-class EpsilonUNet3D(nn.Module):
-    def __init__(self, ch=32):
-        super().__init__()
-        self.conv1 = nn.Conv3d(ch, ch, 3, padding=1)
-        self.conv2 = nn.Conv3d(ch, ch, 3, padding=1)
-        self.conv3 = nn.Conv3d(ch, ch, 3, padding=1)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.utils import timestep_embedding
 
-    def forward(self, x, t):
-        # t is unused for now (we add it later)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        return self.conv3(x)
+
+class ConditionalEpsUNet3D(nn.Module):
+    def __init__(self, z_ch, cond_ch, tdim=128):
+        super().__init__()
+
+        self.t_mlp = nn.Sequential(
+            nn.Linear(tdim, z_ch),
+            nn.SiLU(),
+            nn.Linear(z_ch, z_ch)
+        )
+
+        self.in_conv = nn.Conv3d(z_ch + cond_ch, z_ch, 3, padding=1)
+        self.mid = nn.Conv3d(z_ch, z_ch, 3, padding=1)
+        self.out = nn.Conv3d(z_ch, z_ch, 3, padding=1)
+
+    def forward(self, z, t, cond):
+        """
+        z:    noisy latent
+        cond: conditioning latent
+        t:    timestep tensor [B]
+        """
+
+        t_emb = timestep_embedding(t, self.t_mlp[0].in_features)
+        t_emb = self.t_mlp(t_emb)
+        t_emb = t_emb[:, :, None, None, None]
+
+        x = torch.cat([z, cond], dim=1)
+        x = self.in_conv(x)
+
+        x = x + t_emb
+        x = F.relu(self.mid(x))
+        return self.out(x)
