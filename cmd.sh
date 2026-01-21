4992a990f1bb6e051adf3cb58b392c8c45efd427
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-01-21 12:40:43 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,3 +3,6 @@
 apt update && apt install -y rclone
 apt install -y unzip
 curl https://rclone.org/install.sh | bash
+
+mkdir -p /workspace/dataset
+rclone copy gdrive:IXI-T1 /workspace/dataset --progress
