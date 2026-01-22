2bd0e3edc0fbce22d3367af93082d76c0368948c
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-01-22 11:51:57 +0530

    Create schedule.py

---
+++ Diffusion/schedule.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+class SinusoidalTimeEmbedding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, t):
+        """
+        t: (B,) integer timesteps
+        """
+        half = self.dim // 2
+        emb = math.log(10000) / (half - 1)
+        emb = torch.exp(torch.arange(half, device=t.device) * -emb)
+        emb = t[:, None] * emb[None, :]
+        emb = torch.cat([emb.sin(), emb.cos()], dim=-1)
+        return emb
