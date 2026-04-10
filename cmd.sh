7bd0ba6c5ef3adcaffc4101da2a35951027f7c02
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-04-10 15:38:59 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,6 +3,6 @@
 apt update && apt install -y rclone
 apt install -y unzip
 curl https://rclone.org/install.sh | bash
-
+#make changes to noise balancicng(pred_std) 
 mkdir -p /workspace/dataset
 rclone copy gdrive:IXI-T1 /workspace/dataset --progress
