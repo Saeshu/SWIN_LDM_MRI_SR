9e3fc0bce480319004cb1fb7ed696c1e5163f402
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-06-28 18:21:53 +0530

    Update advanced_losses.py

--- Diffusion/advanced_losses.py
+++ Diffusion/advanced_losses.py
@@ -441,4 +441,5 @@ def compute_losses(
             "mse": mse_loss,
             "recon": recon_loss,
             "res": res_loss,
-            "perc": perc_loss}
+            "perc": perc_loss
+           }
