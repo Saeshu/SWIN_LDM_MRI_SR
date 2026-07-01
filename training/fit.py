31ba345fd519913d2e094369466543f885a3cb43
Saeshu <99483970+Saeshu@users.noreply.github.com>
2026-07-01 14:58:02 +0530

    Update fit.py

--- training/fit.py
+++ training/fit.py
@@ -1,3 +1,5 @@
+from sampling.visualize_latent_ema import visualize_with_ema
+import torch
 def fit(
     self,
     train_loader,
@@ -25,9 +27,9 @@ def fit(
     ##########################################################
     # Epoch loop
     ##########################################################
-
+    sample_hr, sample_lr = next(iter(train_loader))
     for epoch in range(start_epoch, epochs):
-
+        
         print("\n" + "=" * 70)
         print(f"Epoch {epoch+1}/{epochs}")
         print("=" * 70)
@@ -37,7 +39,39 @@ def fit(
         ######################################################
 
         train_stats = self.train_epoch(train_loader)
+        if (epoch + 1) % 5 == 0:      # Change to 5 or 10 later
 
+            print("\nGenerating sample...\n")
+        
+            visualize_with_ema(
+        
+                ema=self.ema,
+        
+                ae=self.ae,
+        
+                noise_sched=self.noise_scheduler,
+        
+                device=self.device,
+        
+                lr=sample_lr,
+        
+                hr=sample_hr,
+        
+                guidance_scale=1.5,
+        
+                title=f"Epoch {epoch+1}",
+        
+            )
+            
+            torch.save(
+            {
+                "epoch": epoch,
+                "unet": self.unet.state_dict(),
+                "ema": self.ema.ema_model.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
+            },
+            f"/workspace/ckpt/sample_epoch_{epoch+1}.pt",
+            )
         ######################################################
         # Validation
         ######################################################
@@ -105,7 +139,9 @@ def fit(
         #     epoch=epoch,
         #     save_dir=self.save_dir,
         # )
-
+        
+        
+            
     ##########################################################
     # Finished
     ##########################################################
